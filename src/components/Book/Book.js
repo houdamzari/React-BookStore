a2a62438c679b40938ba49d0acde394f9567ba75
houdamzari <houdamzari1999@gmail.com>
2023-01-21 02:50:05 +0100

    Setup layout

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -1,14 +1,35 @@
 import React from "react";
+import "./Book.css";
 
-function Book({ type, title, author }) {
+import progressCircles from "../../assets/imgs/progressCircles.png";
+function Book({ type, title, author, progress, chapter }) {
   return (
-    <div className="book__info">
-      <span>{type}</span>
-      <h2>{title}</h2>
-      <span>{author}</span>
-    </div>
-    <div>
-        <FontAwesomeIcon icon="fa-duotone fa-spinner-third" />
+    <div className="book">
+      <div className="book__info">
+        <span className="book__span">{type}</span>
+        <h2>{title}</h2>
+        <span className="book-author">{author}</span>
+
+        <div className="book__info-buttons">
+          <button className="book__info-btn">Comments </button>
+          <span>|</span>
+          <button className="book__info-btn">Remove </button>
+          <span>|</span>
+
+          <button className="book__info-btn">Edit</button>
+        </div>
+      </div>
+      <div className="book__progress">
+        <img src={progressCircles} alt="" className="book__icon" />
+        <h4 className="book__heading">
+          {progress} %<span className="book__span">Completed</span>
+        </h4>
+      </div>
+      <div>
+        <span className="book__current-chapter">CURRENT CHAPTER</span>
+        <h5 className="book__current-heading">{chapter}</h5>
+        <button className="book__current-btn"> UPDATE PROGRESS</button>
+      </div>
     </div>
   );
 }
--- src/components/Books/Books.js
+++ src/components/Books/Books.js
@@ -3,9 +3,49 @@ import Book from "../Book/Book";
 
 function Books(props) {
   return (
-    <>
-      <Book type="Action" title="The Hunger Games" author="Suzanne Collins" />
-    </>
+    <div className="wrapper">
+      <div className="books">
+        <Book
+          type="Action"
+          title="The Hunger Games"
+          author="Suzanne Collins"
+          chapter="Chapter 17"
+          progress="64"
+        />
+        <Book
+          type="Science-Fiction"
+          title="Dune"
+          author="Frank Herbert"
+          chapter="3: `A Lesson Learned`"
+          progress="8"
+        />
+        <Book
+          type="Economy"
+          title="Capital in the Twenty-First Century"
+          author="Suzanne Collins"
+          chapter="Introduction"
+          progress="0"
+        />
+
+        <hr />
+        <h2 className="add__book-heading">Add new Book </h2>
+
+        <div className="add__book">
+          <input
+            type="text"
+            name=""
+            placeholder="Book title"
+            className="add__book-input"
+          />
+          <select name="Action" id="" className="add__book-select">
+            <option value="action">Action</option>
+            <option value="science-fiction">Science-Fiction</option>
+            <option value="economy">Economy</option>
+          </select>
+          <button className="add__book-btn">Add book</button>
+        </div>
+      </div>
+    </div>
   );
 }
 
