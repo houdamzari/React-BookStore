7714cc22384cf7b00b264971e40468793d0a2d1f
houdamzari <houdamzari1999@gmail.com>
2023-01-26 14:51:14 +0100

    Dispatch categories reducer actions

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -1,7 +1,19 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { checkStatus } from "../../redux/categories/categoriesReducer";
 function Categories(props) {
-  return <button>Check Status</button>;
+  const [status, setStatus] = useState("");
+  const dispatch = useDispatch();
+  const handleClick = () => {
+    const { payload } = dispatch(checkStatus());
+    setStatus(payload);
+  };
+  return (
+    <>
+      <button onClick={handleClick}>Check Status</button>
+      <h2>{status}</h2>
+    </>
+  );
 }
 
 export default Categories;
